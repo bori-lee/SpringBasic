2c7428bbb2e025924a423e55ea60290ffb3df559
lsa <tndkdml@gmail.com>
2023-05-04 14:22:52 +0900

    230504 스프링 기본편 1회 완강

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger; // 마이로거의 스콥이 리퀘스트 스콥.. 근데 리퀘스트가 없다?, 왜냐면 생존범위가 고객이들어와서 나갈떄까지라서. 근데.. 지금 httprequest가 안나왔다. 그러면 provider 사용해서 해결.
+    //private final MyLogger myLogger; // myLogger의 스콥이 리퀘스트 스콥.. 근데 리퀘스트가 없다?, 왜냐면 생존범위가 고객이들어와서 나갈떄까지라서. 근데.. 지금 httprequest가 안나왔다. 그러면 provider 사용해서 해결.
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider; // 주입시점에서 주입받을 수 있다.
 
     @RequestMapping("log-demo")
     @ResponseBody //문자 그대로 응답을 보낼 수 있따.
     public String logDemo(HttpServletRequest request) {
+
         String requestURL = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
